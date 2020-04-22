066da77f3a0bbf7825af9758323992a0c2012679
henrycieza <henryciez@hotmail.com>
2020-04-22 02:40:16 +0200

    generando features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { MainComponent } from './core/main/main.component';
+import { FooterComponent } from './core/footer/footer.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
